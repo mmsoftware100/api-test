fee57b614459d235738e0c68e0e2ac0ebd08c3dd
Aung Ko Man <aungkoman@gmail.com>
2023-11-06 22:46:46 +0630

    test copied

---
+++ test_case/dist/index.js
@@ -0,0 +1,31 @@
+import ApiClient from './ApiClient';
+import Error from './model/Error';
+import Pet from './model/Pet';
+import PetsApi from './api/PetsApi';
+
+/**
+ * 
+* @module index
+* @version 1.0.0
+*/
+export {
+/**
+ * The ApiClient constructor.
+ * @property {module:ApiClient}
+ */
+ApiClient,
+/**
+ * The Error model constructor.
+ * @property {module:model/Error}
+ */
+Error,
+/**
+ * The Pet model constructor.
+ * @property {module:model/Pet}
+ */
+Pet,
+/**
+* The PetsApi service constructor.
+* @property {module:api/PetsApi}
+*/
+PetsApi };
