a0b914e477e35968e535df25bbac4415da80800a
Aung Ko Man <aungkoman@gmail.com>
2023-11-06 22:36:24 +0630

    src copied

---
+++ test_case/test/test_two.js
@@ -0,0 +1,37 @@
+
+const chai = require('chai');
+const expect = chai.expect;  // Use your preferred assertion library
+
+
+describe('PetsApi', function() {
+    describe('createPets', function() {
+      it('should call createPets successfully', function(done) {
+        //uncomment below and update the code to test createPets
+        // instance.createPets(function(error) {
+        //  if (error) throw error;
+        // expect().to.be();
+        // });
+        done();
+      });
+    });
+    describe('listPets', function() {
+      it('should call listPets successfully', function(done) {
+        //uncomment below and update the code to test listPets
+        //instance.listPets(function(error) {
+        //  if (error) throw error;
+        //expect().to.be();
+        //});
+        done();
+      });
+    });
+    describe('showPetById', function() {
+      it('should call showPetById successfully', function(done) {
+        //uncomment below and update the code to test showPetById
+        //instance.showPetById(function(error) {
+        //  if (error) throw error;
+        //expect().to.be();
+        //});
+        done();
+      });
+    });
+  });
